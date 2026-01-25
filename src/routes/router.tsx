88c2b7596328334d71d40096014ddc4ce14a1eab
Thiag0-web <thiago.fullstacker@gmail.com>
2026-01-25 09:30:51 -0300

    Redireciona a rota raiz para a listagem de tickets

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -2,13 +2,14 @@ import { BaseLayout } from "@/layouts/baseLayout";
 import { TicketPage } from "@/pages/create-ticket/page";
 import { TicketPageId } from "@/pages/ticket/page";
 import { TicketsPage } from "@/pages/tickets/page";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 export function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<BaseLayout />}>
+        <Route path="/" element={<Navigate to="/tickets" replace />} />
           <Route path="/tickets" element={<TicketsPage />}/>
           <Route path="/tickets/new" element={<TicketPage/>}/>
           <Route path="/tickets/:ticketid" element={<TicketPageId />}/>
